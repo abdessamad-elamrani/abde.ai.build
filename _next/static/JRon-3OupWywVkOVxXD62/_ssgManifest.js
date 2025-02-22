8049bf3836c45b98abdc0f581e88228c96093d93
Abdessamad EL AMRANI <elamrani.abdessamad@gmail.com>
2025-02-22 14:44:35 +0100

    Deploy: 2025-02-22 14:44:35

---
+++ _next/static/JRon-3OupWywVkOVxXD62/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Fevents\u002F[slug]","\u002Ftech-in-motion\u002F[slug]","\u002Ftech-writeups\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
